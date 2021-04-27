b2d607fdb8954c7795cc3cb017ccc4656669a6f8
YurleyTorres <marly.torres.silva@gmail.com>
2021-04-26 21:25:28 -0500

    26-04-2021

---
+++ Ejercicio2.js
@@ -0,0 +1,18 @@
+// 2.	3 usuarios entran a una heladería a comprar un helado, pero hay un problema: los precios no están al lado de cada estante 
+//con su respectivo helado. “Ellos quieren comprar el helado más caro que puedan con la plata que tienen.
+// Miguel tiene $1.000 
+// Carlos tiene $2.000 
+// Manuel tiene $500 
+// Los precios de helados son los siguientes:
+// Bonais: $400
+// Palito de helado de agua: $1.000 
+// Palito de helado de crema: $2.200
+// Bombón helado con arequipe: $1.500
+// Bombón helado con chispas de chocolate: $2.500
+// Bombón helado con fresas: $2.800
+// Medio litro de helado: $ 2.000
+
+// El programa tiene que pedir el ingreso de su dinero y el nombre de cliente
+// Si hay más de un helado con el mismo precio o por de bajo de precio del usuario mostrar las opciones disponibles
+// El programa también tiene que poder dar los vueltos de su compra con su factura
+
